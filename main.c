a1fdfa331728ecaaa1aaa3dbf68ede97dbf5b712
GiusseppeR <giusseppe.rojas@ug.uchile.cl>
2024-05-05 00:22:34 -0400

    FEAT: added a function to generate all the experiments

--- main.c
+++ main.c
@@ -5,27 +5,14 @@
 #include "arrays/array.h"
 #include "random/custom_random.h"
 #include "utils.h"
+#include "query/query.h"
 
 int main(void) {
     Allocator allocator = {my_alloc, my_free, 0};
     splitmix64_init((uint64_t) time(NULL));
     xoshiro256plus_init();
 
-    int n = 33554432;
+    generate_experiment();
 
-    Point *input = array(Point, &allocator);
-
-    for(int i = 0; i < n; i++)
-        array_append(input, generateRandomPoint());
-
-    printf("%i random points of double precision generated. \n",n);
-
-    Tree * tree = ciaccia_patella(input);
-
-    printf("Tree height: %i\n",tree->height);
-    for(int i = 0; i < tree->size; i++)
-        printf("radius %i: %f\n", i, tree->entries[i].radius);
-    free_tree(tree);
-    _getch();
     return 0;
 }
---
+++ query/query.h
@@ -0,0 +1,6 @@
+#ifndef TAREALOG_QUERY_H
+#define TAREALOG_QUERY_H
+#include "../structures.h"
+void query(Tree tree, Point point, double radius, Point ** result, int * accesses);
+void generate_experiment();
+#endif //TAREALOG_QUERY_H
