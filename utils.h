be298a3c8c4a9d2cb60b35dcdf840c7e6585f042
Joyo-G <gustavo.joyo.p@gmail.com>
2024-05-05 15:00:38 -0400

    ADD: compare fuction signature.

--- utils.h
+++ utils.h
@@ -8,5 +8,6 @@
 Point generateRandomPoint();
 double squaredDistance(const Point p1, const Point p2);
 int minDistance(Point point, Point * samples);
+int compare(Point p1, Point p2);
 ClosestPoints closest(Point P[], int n);
 #endif //TAREALOG_UTILS_H
