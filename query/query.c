4063be4e4bd19ce10e3005c9f1ea753093ce21e8
Joyo-G <gustavo.joyo.p@gmail.com>
2024-05-05 20:12:54 -0400

    ADD: ss_algorithm to query

--- query/query.c
+++ query/query.c
@@ -19,13 +19,13 @@ void query(const Tree tree, const Point point, const double radius, Point ** res
     }
 }
 //cambiarle la firma a hundred_queries(Tree tree_cp, Tree tree_ss, int n)
-static void hundred_queries(Tree tree, int n){
+static void hundred_queries(Tree tree_cp, Tree tree_ss  ,int n){
     for(int i = 0; i < 100; i++){
         Point * results_cp = array(Point, &alloc); //arreglo para arbol cp
         int accesses_cp = 0; //accesos en árbol cp
         Point center = generateRandomPoint(); //este punto se usa para ambos árboles!!!
         //Query para árbol cp:
-        query(tree, center, 0.02, &results_cp, &accesses_cp);
+        query(tree_cp, center, 0.02, &results_cp, &accesses_cp);
         printf("Query %i finished:\n",i + 1);
         printf("accesses_cp = %i\n", accesses_cp);
         printf("points = %zu\n", array_length(results_cp));
@@ -35,7 +35,18 @@ static void hundred_queries(Tree tree, int n){
         /*
          * hacer lo mismo para árbol ss, usar el mismo punto "center"
          */
+        Point * results_ss = array(Point, &alloc); //arreglo para arbol ss
+        int accesses_ss = 0; //accesos en árbol ss
+        //Query para árbol ss:
+        query(tree_ss, center, 0.02, &results_ss, &accesses_ss);
+        printf("Query %i finished:\n",i + 1);
+        printf("accesses_ss = %i\n", accesses_ss);
+        printf("points = %zu\n", array_length(results_ss));
+        printf("point percentage = %f \n", (double)array_length(results_ss) / (double)n * 100);
+        printf("\n");
+
         array_free(results_cp);
+        array_free(results_ss);
     }
 }
 
@@ -58,12 +69,14 @@ void generate_experiment(){
         Tree * tree_cp = ciaccia_patella(input);
 
         printf("Tree of height %i built using Ciaccia-Patella.\n", tree_cp->height);
-        /*
-         * generar otro arbol con SS
-         */
+
+        Tree * tree_ss = sexton_swinbank(input);
+
+        printf("Tree of height %i built using Sexton-Swinbank.\n", tree_ss->height);
+
         printf("starting queries...\n");
 
-        hundred_queries(*tree_cp, n);
+        hundred_queries(*tree_cp, *tree_ss, n);
 
         clock_gettime(CLOCK_MONOTONIC,&t2);
 
@@ -73,7 +86,10 @@ void generate_experiment(){
 
         printf("Execution time: %f seconds \n", delta_t);
         printf("\n");
+
         free_tree(tree_cp);
+        free_tree(tree_ss);
+
         array_free(input);
     }
 }
